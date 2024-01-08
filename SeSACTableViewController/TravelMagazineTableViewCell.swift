fb2766adf2e128edb52dccc556fda020a9f9d9ce
leevwe@naver.com <leevwe@naver.com>
2024-01-09 01:50:51 +0900

    240109 Travel Magazine TableViewController 추가

---
+++ SeSACTableViewController/TravelMagazineTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TravelMagazineTableViewCell.swift
+//  SeSACTableViewController
+//
+//  Created by ungq on 1/9/24.
+//
+
+import UIKit
+
+class TravelMagazineTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
